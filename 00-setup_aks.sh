cf7807fb69382b85d7828d5e95afd1fdcc65d362
Vladimír Nahodil <vnahodil@Vladimir-MacBook-Pro.local>
2021-08-20 15:20:04 +0200

    add aks setup script

---
+++ 00-setup_aks.sh
@@ -0,0 +1,7 @@
+## setup AKS
+## Install Azure CLI on Mac with brew update && brew install azure-cli
+
+# Login options https://docs.microsoft.com/en-us/cli/azure/authenticate-azure-cli
+az login
+
+
